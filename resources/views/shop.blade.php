cd75c5b4aae700129364b8ef8e6b67e5fc914814
Fuad2Dev <94391454+Fuad2Dev@users.noreply.github.com>
2022-10-20 04:09:24 +0000

    Update shop.blade.php

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,350 +1,65 @@
 <x-layouts.app page='shop' section='' pageSlug='shop'>
-    <button type="button" class="btn btn-primary position-fixed p-3" style="z-index: 5; right: 10px">
-      <i class="tim-icons icon-cart"></i> <span class="badge badge-light">+4</span>
-    </button>
-    <button type="button" class="btn btn-primary position-fixed p-3" style="z-index: 5; right: 90px">
-      <i class="tim-icons icon-cart"></i> <span class="badge badge-light">view</span>
-    </button>
-    <button type="button" class="btn btn-success position-fixed p-3" style="z-index: 5; right: 10px; bottom: 30%">
-      <i class="tim-icons icon-check-2" style="vertical-align: middle;"></i> <span style="vertical-align: middle;">Save</span>
-    </button>
-    <button type="button" class="btn btn-danger position-fixed p-3" style="z-index: 5; right: 10px; bottom: 15%">
-      <i class="tim-icons icon-simple-remove " style="vertical-align: middle;"></i> <span style="vertical-align: middle;">Cancel</span>
-    </button>
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div class="col">
-            <div class="card  ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="{{ asset('assets/img/dress1.jpg') }}" class="img-fluid w-100" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
 
-                <div class="card-body">
-                    <h5 class="card-title">Brand</h5>
-                    <p class="card-text">
-                        <strong>Size:</strong> <br>
-                        <strong>Price:</strong>
-                    </p>
 
-                    <div class="text-right"><button type="button" class="btn btn-success add_to_cart">Add to Cart</button></div>
+    <form action="" method="post">
+        <button type="button" class="btn btn-primary position-fixed p-3" style="z-index: 5; right: 10px">
+            <i class="tim-icons icon-cart"></i> <span class="badge badge-light">+4</span>
+        </button>
+        <button type="button" class="btn btn-primary position-fixed p-3" style="z-index: 5; right: 90px">
+            <i class="tim-icons icon-cart"></i> <span class="badge badge-light">view</span>
+        </button>
+        <button type="button" class="btn btn-success position-fixed p-3" style="z-index: 5; right: 10px; bottom: 30%">
+            <i class="tim-icons icon-check-2" style="vertical-align: middle;"></i> <span
+                style="vertical-align: middle;">Save</span>
+        </button>
+        <button type="button" class="btn btn-danger position-fixed p-3" style="z-index: 5; right: 10px; bottom: 15%">
+            <i class="tim-icons icon-simple-remove " style="vertical-align: middle;"></i> <span
+                style="vertical-align: middle;">Cancel</span>
+        </button>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            @for ($i = 1; $i <= 10; $i++)
+                <div class="col" style="max-width: 260.328px">
+                    <div class="card  ">
+                        <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                            <img src="{{ asset('assets/img/dress1.jpg') }}" class="img-fluid w-100" />
+                            <a href="#!">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+                            </a>
+                        </div>
+
+                        <div class="card-body">
+                            <h5 class="card-title">Brand</h5>
+                            <p class="card-text">
+                                <strong>Size:</strong> <br>
+                                <strong>Price:</strong>
+                            </p>
+
+                            <div class="text-right">
+                                <input type="checkbox" name="p-{{ $i }}" id="p-{{ $i }}"
+                                    style="display: none">
+                                <button type="button" class="btn btn-success add_to_cart" id="p-{{ $i }}">
+                                    Add to Cart
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            @endfor
         </div>
-        <div class="col">
-            <div class="card  ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="{{ asset('assets/img/dress2.png') }}" class="img-fluid w-100" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
-
-                <div class="card-body">
-                    <h5 class="card-title">Brand</h5>
-                    <p class="card-text">
-                        <strong>Size:</strong> <br>
-                        <strong>Price:</strong>
-                    </p>
-
-                    <div class="text-right"><button type="button" class="btn btn-success add_to_cart">Add to Cart</button></div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card  ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="{{ asset('assets/img/dress1.jpg') }}" class="img-fluid w-100" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
-
-                <div class="card-body">
-                    <h5 class="card-title">Brand</h5>
-                    <p class="card-text">
-                        <strong>Size:</strong> <br>
-                        <strong>Price:</strong>
-                    </p>
-
-                    <div class="text-right"><button type="button" class="btn btn-success add_to_cart">Add to Cart</button></div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card  ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="{{ asset('assets/img/dress2.png') }}" class="img-fluid w-100" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
-
-                <div class="card-body">
-                    <h5 class="card-title">Brand</h5>
-                    <p class="card-text">
-                        <strong>Size:</strong> <br>
-                        <strong>Price:</strong>
-                    </p>
-
-                    <div class="text-right"><button type="button" class="btn btn-success add_to_cart">Add to Cart</button></div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card  ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="{{ asset('assets/img/dress1.jpg') }}" class="img-fluid w-100" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
-
-                <div class="card-body">
-                    <h5 class="card-title">Brand</h5>
-                    <p class="card-text">
-                        <strong>Size:</strong> <br>
-                        <strong>Price:</strong>
-                    </p>
-
-                    <div class="text-right"><button type="button" class="btn btn-success add_to_cart">Add to Cart</button></div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card  ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="{{ asset('assets/img/dress1.jpg') }}" class="img-fluid w-100" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
-
-                <div class="card-body">
-                    <h5 class="card-title">Brand</h5>
-                    <p class="card-text">
-                        <strong>Size:</strong> <br>
-                        <strong>Price:</strong>
-                    </p>
-
-                    <div class="text-right"><button type="button" class="btn btn-success add_to_cart">Add to Cart</button></div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card  ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="{{ asset('assets/img/dress2.png') }}" class="img-fluid w-100" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
-
-                <div class="card-body">
-                    <h5 class="card-title">Brand</h5>
-                    <p class="card-text">
-                        <strong>Size:</strong> <br>
-                        <strong>Price:</strong>
-                    </p>
-
-                    <div class="text-right"><button type="button" class="btn btn-success add_to_cart">Add to Cart</button></div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card  ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="{{ asset('assets/img/shirt1.jpg') }}" class="img-fluid w-100" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
-
-                <div class="card-body">
-                    <h5 class="card-title">Brand</h5>
-                    <p class="card-text">
-                        <strong>Size:</strong> <br>
-                        <strong>Price:</strong>
-                    </p>
-
-                    <div class="text-right"><button type="button" class="btn btn-success add_to_cart">Add to Cart</button></div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card  ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="{{ asset('assets/img/dress3.jpg') }}" class="img-fluid w-100" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
-
-                <div class="card-body">
-                    <h5 class="card-title">Brand</h5>
-                    <p class="card-text">
-                        <strong>Size:</strong> <br>
-                        <strong>Price:</strong>
-                    </p>
-
-                    <div class="text-right"><button type="button" class="btn btn-success add_to_cart">Add to Cart</button></div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card  ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="{{ asset('assets/img/dress2.png') }}" class="img-fluid w-100" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
-
-                <div class="card-body">
-                    <h5 class="card-title">Brand</h5>
-                    <p class="card-text">
-                        <strong>Size:</strong> <br>
-                        <strong>Price:</strong>
-                    </p>
-
-                    <div class="text-right"><button type="button" class="btn btn-success add_to_cart">Add to Cart</button></div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card  ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="{{ asset('assets/img/shirt1.jpg') }}" class="img-fluid w-100" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
-
-                <div class="card-body">
-                    <h5 class="card-title">Brand</h5>
-                    <p class="card-text">
-                        <strong>Size:</strong> <br>
-                        <strong>Price:</strong>
-                    </p>
-
-                    <div class="text-right"><button type="button" class="btn btn-success add_to_cart">Add to Cart</button></div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card  ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="{{ asset('assets/img/dress3.jpg') }}" class="img-fluid w-100" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
-
-                <div class="card-body">
-                    <h5 class="card-title">Brand</h5>
-                    <p class="card-text">
-                        <strong>Size:</strong> <br>
-                        <strong>Price:</strong>
-                    </p>
-
-                    <div class="text-right"><button type="button" class="btn btn-success add_to_cart">Add to Cart</button></div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card  ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="{{ asset('assets/img/shirt1.jpg') }}" class="img-fluid w-100" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
-
-                <div class="card-body">
-                    <h5 class="card-title">Brand</h5>
-                    <p class="card-text">
-                        <strong>Size:</strong> <br>
-                        <strong>Price:</strong>
-                    </p>
-
-                    <div class="text-right"><button type="button" class="btn btn-success add_to_cart">Add to Cart</button></div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card  ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="{{ asset('assets/img/dress3.jpg') }}" class="img-fluid w-100" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
-
-                <div class="card-body">
-                    <h5 class="card-title">Brand</h5>
-                    <p class="card-text">
-                        <strong>Size:</strong> <br>
-                        <strong>Price:</strong>
-                    </p>
-
-                    <div class="text-right"><button type="button" class="btn btn-success add_to_cart">Add to Cart</button></div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card  ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="{{ asset('assets/img/shirt1.jpg') }}" class="img-fluid w-100" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
-
-                <div class="card-body">
-                    <h5 class="card-title">Brand</h5>
-                    <p class="card-text">
-                        <strong>Size:</strong> <br>
-                        <strong>Price:</strong>
-                    </p>
-
-                    <div class="text-right"><button type="button" class="btn btn-success add_to_cart">Add to Cart</button></div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card  ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="{{ asset('assets/img/dress3.jpg') }}" class="img-fluid w-100" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
-
-                <div class="card-body">
-                    <h5 class="card-title">Brand</h5>
-                    <p class="card-text">
-                        <strong>Size:</strong> <br>
-                        <strong>Price:</strong>
-                    </p>
-
-                    <div class="text-right"><button type="button" class="btn btn-success add_to_cart">Add to Cart</button></div>
-                </div>
-            </div>
-        </div>
-    </div>
+    </form>
 
     <x-slot name="js">
         <script>
-            $(function () {
-                $('.add_to_cart').click(function () {
+            $(function() {
+                $('.add_to_cart').click(function() {
                     $card = $(this).parent().parent().parent();
 
                     $card.toggleClass('card-select')
 
                     if ($card.hasClass('card-select')) {
+                        console.log($(this).parent().children('input').prop('checked', true));
                         $(this).text('REMOVE')
-                    }else{
+                    } else {
                         $(this).text('ADD TO CART')
                     }
 
